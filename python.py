2ec217b53eb7b3a49c07ff21076fbf7670a762bf
Pushkaran_Tyagi163 <117625210+Pushkaran163@users.noreply.github.com>
2024-11-20 02:07:13 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+        
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+        
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+        
+animals = [Dog(), Cat(), Animal()]
+for animal in animals:
+    animal.sound()
